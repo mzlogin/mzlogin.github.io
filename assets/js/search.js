685cf60cd2d8935d2c622a54d8664d383630ed34
Zhuang Ma <chumpma@gmail.com>
2016-10-22 16:45:07 +0800

    Search bar in post page match post directory width, #14

--- assets/js/search.js
+++ assets/js/search.js
@@ -61,6 +61,11 @@ jQuery(function() {
     }
 
     function adjust_search_box_width() {
+        if ($(".post-directory").length) {
+            if ($(".post-directory").is(":visible")) {
+                $("#site_search").width(300);
+            }
+        }
         var searchbar_width = $("#site_search").width();
         $("#search_box").width(searchbar_width - 65);
     }
