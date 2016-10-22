cc41bbe5c58dc3b42805ed70252c0c82d9684e9c
Zhuang Ma <chumpma@gmail.com>
2016-10-22 16:03:29 +0800

    Fix #14, auto suit search box width

--- assets/js/search.js
+++ assets/js/search.js
@@ -59,4 +59,15 @@ jQuery(function() {
             }
         });
     }
+
+    function adjust_search_box_width() {
+        var searchbar_width = $("#site_search").width();
+        $("#search_box").width(searchbar_width - 65);
+    }
+
+    adjust_search_box_width();
+
+    $(window).on("resize", function() {
+        adjust_search_box_width();
+    });
 });
