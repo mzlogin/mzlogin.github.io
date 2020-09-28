a99aa61592320eee5cf9aadb55f397247f1c9da2
Zhuang Ma <chumpma@gmail.com>
2020-09-29 04:08:48 +0800

    fix header in blockquote
    fix #73

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -1,5 +1,5 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
-// Updated by http://mazhuang.org
+// Updated by https://mazhuang.org
 (function($){
   $.fn.toc = function(options) {
     var defaults = {
@@ -20,6 +20,9 @@
     }
 
     var headers = $(settings.headers).filter(function() {
+      if ($(this).parent('blockquote').length > 0) {
+        return false;
+      }
       // get all headers with an ID
       var previousSiblingName = $(this).prev().attr( "name" );
       if (!this.id && previousSiblingName) {
